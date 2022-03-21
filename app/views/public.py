74ebca63f5181ed7c9f703e19fe6f4cf1fbd3ab3
jp <joaoooh@gmail.com>
2022-03-21 16:35:42 -0300

    Static and template files

--- app/views/public.py
+++ app/views/public.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask.blueprints import Blueprint
 
 public_bp = Blueprint("public", __name__)
@@ -5,4 +6,4 @@ public_bp = Blueprint("public", __name__)
 @public_bp.route("/", methods=["GET","POST"])
 def home():
     
-    return "home"
+    return render_template("home.html")
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv(path.join(base_dir, ".env"), override=True)
 
 class Config(object):
 
-    SECRET_KEY = "Q*[\\!o\xd6\xe5v\xcd\xaa]F\xfb\xe0\xb0\xe3\x8f\xfb\xcf\xf1j\xa8>"
+    SECRET_KEY = "*q[\\O!\xd6\xe5v\xcd\xaaJ]\xfb\xe0\xb0\xe3\x8f\xfb\xcf\xf1f\xa8>"
     FLASK_APP = "wsgi.py"
 
     DEBUG = True
@@ -17,7 +17,7 @@ class Config(object):
     TEMPLATES_FOLDER = "templates"
 
     SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://jpmol:J0$3m3nd0nça123@jpmol.mysql.pythonanywhere-services.com/tictactoe"
+        "mysql+pymysql://jpmol:kw1k1kjkwj@jpmol.mysql.pythonanywhere-services.com/jpmol$tictactoe"
     )
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
